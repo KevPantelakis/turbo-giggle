d0ec0dace395cb585e21ad0c43ea953ce98d1074
KevPantelakis <kev.pantelakis@gmail.com>
2016-11-21 12:39:47 -0500

    tp52610

---
+++ LOG2410/tp5/TP5-A16/Code/CircuitLiqComposite.cpp
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////
+//  CircuitLiqComposite.cpp
+//  Implementation of the Class CircuitLiqComposite
+//  Created on:      27-oct.-2016 15:19:51
+//  Original author: francois
+///////////////////////////////////////////////////////////
+
+#include <stdexcept>
+
+#include "CircuitLiqComposite.h"
+
+
+CircuitLiqComposite::CircuitLiqComposite(){
+
+}
+
+CircuitLiqComposite::~CircuitLiqComposite(){
+
+}
+
+ElmCircuitLiquide::ElmCircuitLiqPtr CircuitLiqComposite::getSousElement( size_t index ){
+
+	if (index < 0 || index >= m_CircuitLiquide.size())
+		throw std::range_error( "CircuitLiqComposite::getSousElement: indexation a l'exterieur du vecteur d'element" );
+	return  m_CircuitLiquide[index];
+}
+
+const ElmCircuitLiquide::ElmCircuitLiqPtr CircuitLiqComposite::getSousElement(size_t index) const {
+
+	if (index < 0 || index >= m_CircuitLiquide.size())
+		throw std::range_error("CircuitLiqComposite::getSousElement: indexation a l'exterieur du vecteur d'element");
+	return  m_CircuitLiquide[index];
+}
+
+int CircuitLiqComposite::nombreSousElements() const {
+
+	return m_CircuitLiquide.size();
+}
+
+void CircuitLiqComposite::addSousElement(const ElmCircuitLiqPtr& sousElm){
+	m_CircuitLiquide.push_back(sousElm);
+}
+
+
+void CircuitLiqComposite::addSousElement(ElmCircuitLiquide* sousElm){
+	m_CircuitLiquide.push_back(ElmCircuitLiqPtr(sousElm));
+}
+
+float CircuitLiqComposite::getTartre(void) const {
+	int nse = nombreSousElements();
+	float tartreMax = 0;
+	ElmCircuitLiqPtr sep;
+	float tartre;
+	for (int ise = 0; ise < nse; ++ise)
+	{
+		sep = getSousElement(ise);
+		tartre = sep->getTartre();
+		tartreMax = (tartre>tartreMax) ? tartre : tartreMax;
+	}
+	return tartreMax;
+}
+
+void CircuitLiqComposite::operer(float duree){
+	int nse = nombreSousElements();
+	ElmCircuitLiqPtr sep;
+	for (int ise = 0; ise < nse; ++ise)
+	{
+		sep = getSousElement(ise);
+		sep->operer(duree);
+	}
+}
+
+void CircuitLiqComposite::nettoyer(){
+	int nse = nombreSousElements();
+	ElmCircuitLiqPtr sep;
+	for (int ise = 0; ise < nse; ++ise)
+	{
+		sep = getSousElement(ise);
+		sep->nettoyer();
+	}
+}
+
+
---
+++ LOG2410/tp5/TP5-A16/Code/CircuitSolComposite.cpp
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////
+//  CircuitSolComposite.cpp
+//  Implementation of the Class CircuitSolComposite
+//  Created on:      27-oct.-2016 15:12:34
+//  Original author: francois
+///////////////////////////////////////////////////////////
+
+#include <stdexcept>
+
+#include "CircuitSolComposite.h"
+
+
+CircuitSolComposite::CircuitSolComposite(){
+
+}
+
+
+
+CircuitSolComposite::~CircuitSolComposite(){
+
+}
+
+
+CircuitSolComposite::ElmCircuitSolPtr CircuitSolComposite::getSousElement(size_t index){
+
+	if (index < 0 || index >= m_CircuitSolide.size())
+		throw std::range_error("CircuitSolComposite::getSousElement: indexation a l'exterieur du vecteur d'element");
+	return  m_CircuitSolide[index];
+}
+
+const CircuitSolComposite::ElmCircuitSolPtr CircuitSolComposite::getSousElement(size_t index) const {
+
+	if (index < 0 || index >= m_CircuitSolide.size())
+		throw std::range_error("CircuitSolComposite::getSousElement: indexation a l'exterieur du vecteur d'element");
+	return  m_CircuitSolide[index];
+}
+
+int CircuitSolComposite::nombreSousElements() const {
+	return m_CircuitSolide.size();
+}
+
+
+void CircuitSolComposite::addSousElement(const ElmCircuitSolPtr& sousElem){
+	m_CircuitSolide.push_back(sousElem);
+}
+
+void CircuitSolComposite::addSousElement(ElmCircuitSolide* sousElem){
+	m_CircuitSolide.push_back(ElmCircuitSolPtr(sousElem));
+}
+
+float CircuitSolComposite::getDebris(void) const {
+	int nse = nombreSousElements();
+	float debrisMax = 0;
+	float debris = 0;
+	ElmCircuitSolPtr sep;
+	for (int ise = 0; ise < nse; ++ise) {
+		sep = getSousElement(ise);
+		debris = sep->getDebris();
+		debrisMax = (debris>debrisMax) ? debris : debrisMax;
+	}
+	return debrisMax;
+}
+
+void CircuitSolComposite::operer( float duree ){
+	int nse = nombreSousElements();
+	ElmCircuitSolPtr sep;
+	for (int ise = 0; ise < nse; ++ise) {
+		sep = getSousElement(ise);
+		sep->operer(duree);
+	}
+}
+
+void CircuitSolComposite::nettoyer(){
+	int nse = nombreSousElements();
+	ElmCircuitSolPtr sep;
+	for (int ise = 0; ise < nse; ++ise) {
+		sep = getSousElement(ise);
+		sep->nettoyer();
+	}
+}
+
+
